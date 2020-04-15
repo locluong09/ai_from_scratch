8f4f2d7059be5d9af1f39ecb5287082543f359be
Loc Luong <loc.luong09@gmail.com>
2020-04-14 22:09:23 -0600

    Compare different optimizers

--- ai/neural_net_example.py
+++ ai/neural_net_example.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
 from utils.data_preprocessing import min_max_scaler
 from utils.model_selection import train_test_split
@@ -13,12 +15,29 @@ from deep_learning.loss_functions import SquareLoss
 from utils.data_manipulation import R_square, mean_squared_error
 
 
+# from keras.optimizers import Adam, Nadam, Adamax
+def get_data(file_name):
+	dirname = 'data'
+	path = os.path.join(dirname, file_name)
+	data = pd.read_csv(path)
+	return data
+
+def preprocessing(data):
+	column_train = ["ON_STREAM_HRS", "AVG_DOWNHOLE_PRESSURE", "AVG_DOWNHOLE_TEMPERATURE",
+	"AVG_CHOKE_SIZE_P", "AVG_WHP_P", "AVG_WHT_P"]
+	target = ["BORE_OIL_VOL"]
+	X = data[column_train].to_numpy()
+	y = data[target].to_numpy()
+	return X, y
+
+
 
 def main():
 	#load data
 	X = np.loadtxt("data/input.txt")
 	y = np.loadtxt("data/output.txt")
-
+	# data = get_data("data.csv")
+	# X,y = preprocessing(data)
 	n_samples, n_features = X.shape
 
 	X = min_max_scaler(X)
@@ -27,29 +46,36 @@ def main():
 	X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, 0.1, shuffle = True, seed = 1000)
 	validation_data = (X_val, y_val)
 
-	GD = GradientDescent(0.01)
-	SGD = StochasticGradientDescent(learning_rate = 0.01, momentum = 0.9, nesterov = True)
-	Ada = Adagrad(learning_rate = 0.01, epsilon = 1e-6)
+	GD = GradientDescent(0.001)
+	SGD = StochasticGradientDescent(learning_rate = 0.001, momentum = 0.9, nesterov = False)
+	SGD_nes = StochasticGradientDescent(learning_rate = 0.001, momentum = 0.9, nesterov = True)
+	Ada = Adagrad(learning_rate = 0.001, epsilon = 1e-6)
 	Adad = Adadelta(rho = 0.9, epsilon = 1e-6)
 	RMS = RMSProp(learning_rate = 0.01)
 	Adam_opt = Adam(learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-6)
 	Adamax_opt = Adam(learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-6)
-<<<<<<< HEAD
-	NAdam_opt = Adam(learning_rate = 0.005, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-6)
-=======
 	NAdam_opt = Adam(learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-6)
->>>>>>> master
-	model = Neural_Networks(optimizer = NAdam_opt,
+	NAdam_opt = Adam(learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-6)
+	
+	model = Neural_Networks(optimizer = Adam_opt,
 							loss = SquareLoss,
 							validation_data = validation_data)
 	model.add(Dense(200, input_shape=(n_features,)))
 	model.add(Activation('sigmoid'))
+	model.add(Dense(100))
+	model.add(Activation('tanh'))
 	model.add(Dense(100))
 	model.add(Activation('sigmoid'))
 	model.add(Dense(2))
 	model.add(Activation('linear'))
 
 	train_err, val_err = model.fit(X_train,y_train, n_epochs = 500, batch_size=8)
+	print(len(train_err))
+	print(len(val_err))
+
+	print("Training and validate errors", train_err[-1], val_err[-1])
+	SGD_err = np.concatenate((np.array(train_err).reshape(-1,1), np.array(val_err).reshape(-1,1)), axis = 1)
+	np.savetxt("Adam_err.txt", SGD_err, delimiter = ',')
 
 	y_train_pred = model.predict(X_train)
 	print("===Training results===")
@@ -74,36 +100,36 @@ def main():
 	print("Overal blind test error", (mean_squared_error(y_pred[0], y_test[0]) + \
 		mean_squared_error(y_pred[1], y_test[1]))/2 )
 
-	plt.plot(train_err, 'r', label = "training")
-	plt.plot(val_err, 'b', label = 'validation')
-	plt.xlabel("Iterations")
-	plt.ylabel("Error")
-	plt.legend()
-	plt.show()
+	# plt.plot(train_err, 'r', label = "training")
+	# plt.plot(val_err, 'b', label = 'validation')
+	# plt.xlabel("Iterations")
+	# plt.ylabel("Error")
+	# plt.legend()
+	# plt.show()
 	
-	plt.plot(np.arange(len(y_pred)), y_pred[:,0], 'r', label = 'y1 predict')
-	plt.plot(np.arange(len(y_pred)), y_test[:,0], 'b', label = 'y1 actual')
-	plt.plot(np.arange(len(y_pred)), y_pred[:,1], 'g', label = 'y2 predict')
-	plt.plot(np.arange(len(y_pred)), y_test[:,1], 'k', label = 'y2 actual')
-	plt.title("Result of blind test")
-	plt.legend()
-	plt.show()
+	# plt.plot(np.arange(len(y_pred)), y_pred[:,0], 'r', label = 'y1 predict')
+	# plt.plot(np.arange(len(y_pred)), y_test[:,0], 'b', label = 'y1 actual')
+	# plt.plot(np.arange(len(y_pred)), y_pred[:,1], 'g', label = 'y2 predict')
+	# plt.plot(np.arange(len(y_pred)), y_test[:,1], 'k', label = 'y2 actual')
+	# plt.title("Result of blind test")
+	# plt.legend()
+	# plt.show()
 
-	plt.plot(np.arange(len(y_train_pred)), y_train_pred[:,0], 'r', label = 'y1 training predict')
-	plt.plot(np.arange(len(y_train)), y_train[:,0], 'b', label = 'y1 training actual')
-	plt.plot(np.arange(len(y_train)), y_train_pred[:,1], 'g', label = 'y2 training predict')
-	plt.plot(np.arange(len(y_train)), y_train[:,1], 'k', label = 'y2 training actual')
-	plt.title("Result of traing set")
-	plt.legend()
-	plt.show()
+	# plt.plot(np.arange(len(y_train_pred)), y_train_pred[:,0], 'r', label = 'y1 training predict')
+	# plt.plot(np.arange(len(y_train)), y_train[:,0], 'b', label = 'y1 training actual')
+	# plt.plot(np.arange(len(y_train)), y_train_pred[:,1], 'g', label = 'y2 training predict')
+	# plt.plot(np.arange(len(y_train)), y_train[:,1], 'k', label = 'y2 training actual')
+	# plt.title("Result of traing set")
+	# plt.legend()
+	# plt.show()
 
-	plt.plot(np.arange(len(y_val)), y_val_pred[:,0], 'r', label = 'y1 val predict')
-	plt.plot(np.arange(len(y_val)), y_val[:,0], 'b', label = 'y1 val actual')
-	plt.plot(np.arange(len(y_val)), y_val_pred[:,1], 'g', label = 'y2 val predict')
-	plt.plot(np.arange(len(y_val)), y_val[:,1], 'k', label = 'y2  val actual')
-	plt.title("Result of validation set")
-	plt.legend()
-	plt.show()
+	# plt.plot(np.arange(len(y_val)), y_val_pred[:,0], 'r', label = 'y1 val predict')
+	# plt.plot(np.arange(len(y_val)), y_val[:,0], 'b', label = 'y1 val actual')
+	# plt.plot(np.arange(len(y_val)), y_val_pred[:,1], 'g', label = 'y2 val predict')
+	# plt.plot(np.arange(len(y_val)), y_val[:,1], 'k', label = 'y2  val actual')
+	# plt.title("Result of validation set")
+	# plt.legend()
+	# plt.show()
 if __name__ == "__main__":
 	main()
 
